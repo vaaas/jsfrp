3e83a96a4c2485b5ff41722c7507800a60ca2120
Vasileios Pasialiokis <pasivasi@outlook.com>
2022-02-16 15:15:45 +0200

    add support for various functions

--- index.js
+++ index.js
@@ -1,21 +1,79 @@
-function Variable(f=undefined) {
-	return Object.assign(f || set(), { x: undefined, ws: [] }, Variable.prototype)
+function Reactive(f, obj={}) {
+	return Object.assign(f, { ws: [], constructor: Reactive, ...obj }, proto)
+}
+
+export const Variable = (x=undefined) => {
+	const r = Reactive(set(), { x: undefined })
+	if (x !== undefined)
+		setTimeout(() => r(x), 0)
+	return r
+}
+
+export const Observable = f => Reactive(f)
+
+export function zip(...rs) {
+	const me = debounce(0)
+
+	const xs = []
+	for (let i = 0, len = rs.length; i < len; i++) {
+		xs.push(undefined)
+		rs[i].each(x => { xs[i] = x; me(xs) })
+	}
+	return me
+}
+
+export function E(name, attrs={}, children=[]) {
+	const elem = document.createElement(name)
+
+	for (const [k, v] of Object.entries(attrs)) {
+		switch(k) {
+		case 'class':
+			if (Array.isArray(v))
+				elem.className = v.join(' ')
+			else if (v.constructor === Reactive)
+				v.each(x => elem.className = Array.isArray(x) ? x.join(' ') : x)
+			else
+				elem.classname = v
+			break
+
+		default:
+			if (v.constructor === Reactive)
+				v.each(x => elem[k] = x)
+			else
+				elem[k] = v
+			break
+		}
+	}
+
+	for (const x of children) {
+		if (typeof x === 'string')
+			elem.appendChild(document.createTextNode(x))
+		else if (typeof x === 'number')
+			elem.appendChild(document.createTextNode(''+x))
+		else if (x.constructor === Reactive) {
+			const node = document.createTextNode('')
+			x.each(x => node.textcontent = x)
+			elem.appendChild(node)
+		}
+	}
+
+	return elem
 }
 
 function set() {
-	return function me(x) { return me.set(x) }
+	return function me(x) { return me.notify(me.x = x) }
 }
 
 function map(f) {
-	return function me(x) { return me.set(f(x)) }
+	return function me(x) { return me.notify(f(x)) }
 }
 
 function filter(f) {
-	return function me(x) { return f(x) ? me.set(x) : this }
+	return function me(x) { return f(x) ? me.notify(x) : this }
 }
 
 function scan(f, i) {
-	return function me(x) { return me.set(i = f(i, x)) }
+	return function me(x) { return me.notify(i = f(i, x)) }
 }
 
 function debounce(t) {
@@ -23,7 +81,7 @@ function debounce(t) {
 
 	function bounce() {
 		id = undefined
-		return me.set(val)
+		return me.notify(val)
 	}
 
 	function me(x) {
@@ -35,48 +93,31 @@ function debounce(t) {
 	return me
 }
 
-function zip(...rs) {
-	const me = debounce(0)
-
-	const xs = []
-	for (let i = 0, len = rs.length; i < len; i++) {
-		xs.push(undefined)
-		rs[i].subscribe(x => { xs[i] = x; me(xs) })
-	}
-	return me
-}
-
-Variable.prototype = {
-	subscribe(x) {
+const proto = {
+	each(x) {
 		this.ws.push(x)
 		return this
 	},
 
 	observe(x) {
-		x.subscribe(this)
+		x.each(this)
 		return this
 	},
 
-	notify() {
+	notify(x) {
 		for (let i = 0, len = this.ws.length; i < len; i++)
-			this.ws[i](this.x)
+			this.ws[i](x)
 		return this
 	},
 
-	set(x) {
-		this.x = x
-		return this.notify()
-	},
-
-	each(f) { return this.subscribe(f) },
-	map(f) { return new Variable(map(f)).observe(this) },
-	filter(f) { return new Variable(filter(f)).observe(this) },
-	scan(f, i) { return new Variable(scan(f, i)).observe(this) },
-	debounce(t) { return new Variable(debounce(t)).observe(this) },
-	zip(...rs) { return new Variable(zip(this, ...rs)) },
+	map(f) { return Observable(map(f)).observe(this) },
+	filter(f) { return Observable(filter(f)).observe(this) },
+	scan(f, i) { return Observable(scan(f, i)).observe(this) },
+	debounce(t) { return Observable(debounce(t)).observe(this) },
+	zip(...rs) { return Observable(zip(this, ...rs)) },
 
 	merge(...rs) {
-		const x = Variable().observe(this)
+		const x = Observable().observe(this)
 		for (let i = 0, len = rs.length; i < len; i++)
 			x.observe(rs[i])
 		return x
